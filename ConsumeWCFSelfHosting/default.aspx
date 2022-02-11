6adcca7de0b517bbf49c6cb3c8007c0e060c7bb4
Murali Krishnamurthy <muralimk@outlook.com>
2022-02-11 13:19:33 +0530

    Add project files.

---
+++ ConsumeWCFSelfHosting/default.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="ConsumeWCFSelfHosting._default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox><br />
+            <asp:Label ID="Label2" runat="server" Text="Label"></asp:Label>
+            <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox><br />
+            <asp:Button ID="btnAdd" runat="server" Text="Add" OnClick="btnAdd_Click" />
+            <asp:Button ID="btnConcat" runat="server" Text="Concat" OnClick="btnConcat_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ ConsumeWCFSelfHosting/default.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ConsumeWCFSelfHosting
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(TextBox1.Text);
+            int b = Convert.ToInt32(TextBox2.Text);
+            calService.CalcServiceClient service = new calService.CalcServiceClient("BasicHttpBinding_ICalcService");
+            Response.Write(service.ConcatIntValue(a, b));
+
+        }
+
+        protected void btnConcat_Click(object sender, EventArgs e)
+        {
+            calService.CalcServiceClient service = new calService.CalcServiceClient("BasicHttpBinding_ICalcService");
+            Response.Write(service.ConcatStringValue(TextBox1.Text,TextBox2.Text));
+        }
+    }
+}
